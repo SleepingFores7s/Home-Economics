9493224f51e80e71353ed2a65d48cb9b7d9e1da1
SleepingFores7s - Laptop <Daniel.Evensson@hotmail.com>
2026-06-08 22:45:56 +0200

    Initial Commit

---
+++ src/main/java/com/example/homeeconomics/HomeEconomicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeeconomics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeEconomicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeEconomicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homeeconomics/HomeEconomicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeeconomics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeEconomicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

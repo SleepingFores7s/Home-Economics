71babd746dfca6656cc58cd53da0e75eff9df92a
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-06-18 04:04:57 +0200

    Changed dto to use LocalDateTime and not Date

--- src/main/java/com/example/homeeconomics/economic/user/dto/UserResponseDto.java
+++ src/main/java/com/example/homeeconomics/economic/user/dto/UserResponseDto.java
@@ -1,10 +1,10 @@
 package com.example.homeeconomics.economic.user.dto;
 
-import java.sql.Date;
+import java.time.LocalDateTime;
 
 public record UserResponseDto(
         String username,
         String email,
-        Date createdAt,
-        Date updatedAt) {
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt) {
 }

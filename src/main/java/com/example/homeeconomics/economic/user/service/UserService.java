9cef1ec97a261278bdf0371fe75c4674c5d6732a
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-06-24 04:14:19 +0200

    Created delete user method and a method for separating out if a user used email or username

--- src/main/java/com/example/homeeconomics/economic/user/service/UserService.java
+++ src/main/java/com/example/homeeconomics/economic/user/service/UserService.java
@@ -1,11 +1,11 @@
 package com.example.homeeconomics.economic.user.service;
 
-import com.example.homeeconomics.economic.user.dto.RegisterUserDto;
+import com.example.homeeconomics.economic.user.dto.UserDeleteDto;
+import com.example.homeeconomics.economic.user.dto.UserRegisterDto;
 import com.example.homeeconomics.economic.user.dto.UserLoginDto;
 import com.example.homeeconomics.economic.user.dto.UserResponseDto;
 import com.example.homeeconomics.economic.user.entity.User;
 import com.example.homeeconomics.economic.user.repository.UserRepository;
-import jakarta.validation.Valid;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,7 +21,7 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public UserResponseDto registerUser(RegisterUserDto dto) {
+    public UserResponseDto registerUser(UserRegisterDto dto) {
 
         return new UserResponseDto(userRepository.save(
                 new User(
@@ -37,13 +37,7 @@ public class UserService {
 
         String userInput = userLoginDto.getUsernameOrEmail();
 
-        User user;
-
-        if (userInput.contains("@")) {
-            user = userRepository.findByEmail(userInput);
-        } else {
-            user = userRepository.findByUsername(userInput);
-        }
+        User user = getUserByLoginIdentifier(userInput);
 
         if (user == null || !passwordEncoder.matches(userLoginDto.getPassword(), user.getPassword())) {
             throw new BadCredentialsException("Invalid username/email or password");
@@ -51,6 +45,27 @@ public class UserService {
 
         return new UserResponseDto(user);
 
+    }
+
+    public void deleteUser(UserDeleteDto deleteDto) {
+
+        String userInput = deleteDto.getUsernameOrEmail();
+
+        User user = getUserByLoginIdentifier(userInput);
+
+        if (passwordEncoder.matches(deleteDto.getPassword(), user.getPassword())) {
+            userRepository.delete(user);
+        }else {
+            throw new BadCredentialsException("Invalid username/email or password");
+        }
+    }
+
+    public User getUserByLoginIdentifier(String emailOrUsername) {
+        if (emailOrUsername.contains("@")) {
+            return userRepository.findByEmail(emailOrUsername);
+        } else {
+            return userRepository.findByUsername(emailOrUsername);
+        }
     }
 
 }

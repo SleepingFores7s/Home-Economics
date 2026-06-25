8a7192c915ff33ef92d7e87b0d000a6b4f6e2186
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-06-25 04:43:56 +0200

    Refactored to return user id

--- src/main/java/com/example/homeeconomics/economic/user/dto/UserResponseDto.java
+++ src/main/java/com/example/homeeconomics/economic/user/dto/UserResponseDto.java
@@ -5,13 +5,16 @@ import com.example.homeeconomics.economic.user.entity.User;
 import java.time.LocalDateTime;
 
 public record UserResponseDto(
+        Long id,
         String username,
         String email,
         LocalDateTime createdAt,
         LocalDateTime updatedAt)
 {
     public UserResponseDto(User user) {
-        this(user.getUsername(),
+        this(
+                user.getId(),
+                user.getUsername(),
                 user.getEmail(),
                 user.getCreatedAt(),
                 user.getUpdatedAt()
